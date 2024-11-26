fba021502e7c75bfce20daa822e73f8f46744b5a
CHECKago <mfalcon@mail.ru>
2024-11-27 00:38:35 +0300

    first

--- ERP/settings.py
+++ ERP/settings.py
@@ -188,6 +188,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-mimetypes.add_type("text/css", ".css", True)
