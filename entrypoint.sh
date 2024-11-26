feae61afb6828f1abe54bdbaf43c096e9496e19f
CHECKago <mfalcon@mail.ru>
2024-11-27 00:51:10 +0300

    first

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@
 # Выполнение миграций и других необходимых команд
 python manage.py migrate --run-syncdb
 #python manage.py loaddata dbdump.json
-#python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 gunicorn ERP.wsgi:application --bind 0.0.0.0:8080 --reload -w 4
