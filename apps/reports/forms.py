90041c3b52885d819d31e027a5d4b7f5552e39a5
CHECKago <mfalcon@mail.ru>
2024-12-05 18:39:32 +0300

    event edit modal form

--- apps/reports/forms.py
+++ apps/reports/forms.py
@@ -27,7 +27,7 @@ class EventForm(forms.ModelForm):
             'invalids': forms.NumberInput(attrs={'class': 'form-control'}),
             'out_of_station': forms.NumberInput(attrs={'class': 'form-control'}),
             'as_part': forms.Select(attrs={'class': 'form-select'}),
-            'paid': forms.CheckboxInput(attrs={'class': 'form-check-input', 'type': 'checkbox'}),
+            'paid': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
             'note': forms.Textarea(attrs={'class': 'form-control', 'rows': '1'}),
         }
 
