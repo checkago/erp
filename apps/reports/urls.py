acd5bae4cd8af4ab73d1612b88a20146f157dab3
CHECKago <mfalcon@mail.ru>
2024-12-05 19:12:20 +0300

    event edit modal form

--- apps/reports/urls.py
+++ apps/reports/urls.py
@@ -2,12 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
-from .views import EventListView, DiaryView
-
+from .views import EventListView, DiaryView, EventUpdateView
 
 urlpatterns = [
     path('diary/', DiaryView.as_view(), name='diary'),
     path('events/', EventListView.as_view(), name='events_list'),
+    path('events/update/<int:pk>/', EventUpdateView.as_view(), name='update_event'),
 ]
 
 
