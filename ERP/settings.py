50be9dc6b2816fad68120d2bc0d1ec075fc33a72
CHECKago <mfalcon@mail.ru>
2024-12-25 14:36:51 +0300

    event edit modal form

--- ERP/settings.py
+++ ERP/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'import_export',
     'rest_framework',
     'crispy_forms',
     "crispy_bootstrap5",
