a166a33c0af2e911fba3506c155da75714a0ebdc
CHECKago <mfalcon@mail.ru>
2025-01-05 19:13:54 +0300

    event edit modal form

--- apps/reports/checks.py
+++ apps/reports/checks.py
@@ -1,27 +1,29 @@
 from django.db.models import Q
 from django.utils import timezone
-from datetime import timedelta
 from .models import VisitReport, BookReport, Event, Branch
 
-CHECK_DAYS = 3
-BAD_LIBRARY_THRESHOLD = 5
+CHECK_DAYS = 1
+BAD_LIBRARY_THRESHOLD = 3
 
 def check_data_fillings():
-    target_date = timezone.now() - timedelta(days=CHECK_DAYS)
-    target_date_only = target_date.date()  # Преобразуем в date
+    # Устанавливаем стартовую дату на 3 января текущего года
+    start_date = timezone.now().replace(month=1, day=3, year=timezone.now().year).date()  # Преобразуем в date
+    current_date = timezone.now().date()  # Текущая дата как date
+    target_date_only = start_date  # Преобразуем в date
 
     results = []
     branches = Branch.objects.filter(Q(adult=True) | Q(child=True) | Q(mod_lib=True))
 
     for branch in branches:
-        latest_visit_report = branch.visitreports.latest('date') if branch.visitreports.exists() else None
-        latest_book_report = branch.bookreports.latest('date') if branch.bookreports.exists() else None
-        latest_event = branch.events.latest('date') if branch.events.exists() else None
+        # Получаем последние отчеты с учетом стартовой даты
+        latest_visit_report = branch.visitreports.filter(date__gte=start_date).last()
+        latest_book_report = branch.bookreports.filter(date__gte=start_date).last()
+        latest_event = branch.events.filter(date__gte=start_date).last()
 
-        # Вычисляем общее количество пропущенных дней с последней записи
-        total_missed_days_visits = (timezone.now().date() - latest_visit_report.date).days if latest_visit_report else 0
-        total_missed_days_books = (timezone.now().date() - latest_book_report.date).days if latest_book_report else 0
-        total_missed_days_events = (timezone.now().date() - latest_event.date).days if latest_event else 0
+        # Вычисляем общее количество пропущенных дней с последней записи или с начала года
+        total_missed_days_visits = (current_date - latest_visit_report.date).days if latest_visit_report else (current_date - start_date).days
+        total_missed_days_books = (current_date - latest_book_report.date).days if latest_book_report else (current_date - start_date).days
+        total_missed_days_events = (current_date - latest_event.date).days if latest_event else (current_date - start_date).days
 
         # Проверяем, есть ли пропущенные дни больше CHECK_DAYS
         if total_missed_days_visits > CHECK_DAYS or total_missed_days_books > CHECK_DAYS or total_missed_days_events > CHECK_DAYS:
