a9a43ceb854c32bddfe217d98d06f754fa4eaaa4
CHECKago <mfalcon@mail.ru>
2025-01-06 22:21:24 +0300

    event edit modal form

--- apps/reports/checks.py
+++ apps/reports/checks.py
@@ -6,18 +6,18 @@ CHECK_DAYS = 3
 BAD_LIBRARY_THRESHOLD = 7
 
 def check_data_fillings():
-    # Устанавливаем стартовую дату на 3 января текущего года
-    start_date = timezone.now().replace(month=1, day=3, year=timezone.now().year).date()
+    # Устанавливаем текущую дату и первый день текущего года
     current_date = timezone.now().date()
+    start_of_year = current_date.replace(month=1, day=1)
 
     results = []
     branches = Branch.objects.filter(Q(adult=True) | Q(child=True) | Q(mod_lib=True))
 
     for branch in branches:
-        # Получаем последние отчеты
-        latest_visit_report = branch.visitreports.last()
-        latest_book_report = branch.bookreports.last()
-        latest_event = branch.events.last()
+        # Получаем последние отчеты только за текущий год
+        latest_visit_report = branch.visitreports.filter(date__gte=start_of_year).last()
+        latest_book_report = branch.bookreports.filter(date__gte=start_of_year).last()
+        latest_event = branch.events.filter(date__gte=start_of_year).last()
 
         # Определяем даты последней записи для каждой категории
         last_visit_date = latest_visit_report.date if latest_visit_report else None
@@ -25,16 +25,16 @@ def check_data_fillings():
         last_event_date = latest_event.date if latest_event else None
 
         # Определяем дату для проверки задержки
-        check_date_visits = last_visit_date if last_visit_date and last_visit_date >= start_date else start_date
-        check_date_books = last_book_date if last_book_date and last_book_date >= start_date else start_date
-        check_date_events = last_event_date if last_event_date and last_event_date >= start_date else start_date
+        check_date_visits = last_visit_date if last_visit_date else start_of_year
+        check_date_books = last_book_date if last_book_date else start_of_year
+        check_date_events = last_event_date if last_event_date else start_of_year
 
         # Вычисляем общее количество пропущенных дней с последней записи или с начала года
         total_missed_days_visits = (current_date - check_date_visits).days
         total_missed_days_books = (current_date - check_date_books).days
         total_missed_days_events = (current_date - check_date_events).days
 
-        # Проверка на будущие даты для всех типов отчетов
+        # Проверка на будущие даты для всех типов отчетов за текущий год
         future_visits = VisitReport.objects.filter(library=branch, date__gt=current_date)
         future_books = BookReport.objects.filter(library=branch, date__gt=current_date)
         future_events = Event.objects.filter(library=branch, date__gt=current_date)
