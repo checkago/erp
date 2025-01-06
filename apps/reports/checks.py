d5fce29f5cc1ff23cb61eb49c90a5d8fef32e0ef
CHECKago <mfalcon@mail.ru>
2025-01-06 18:37:48 +0300

    event edit modal form

--- apps/reports/checks.py
+++ apps/reports/checks.py
@@ -2,14 +2,13 @@ from django.db.models import Q
 from django.utils import timezone
 from .models import VisitReport, BookReport, Event, Branch
 
-CHECK_DAYS = 2
-BAD_LIBRARY_THRESHOLD = 6
+CHECK_DAYS = 3
+BAD_LIBRARY_THRESHOLD = 5
 
 def check_data_fillings():
     # Устанавливаем стартовую дату на 3 января текущего года
     start_date = timezone.now().replace(month=1, day=3, year=timezone.now().year).date()  # Преобразуем в date
     current_date = timezone.now().date()  # Текущая дата как date
-    target_date_only = start_date  # Преобразуем в date
 
     results = []
     branches = Branch.objects.filter(Q(adult=True) | Q(child=True) | Q(mod_lib=True))
@@ -20,10 +19,21 @@ def check_data_fillings():
         latest_book_report = branch.bookreports.filter(date__gte=start_date).last()
         latest_event = branch.events.filter(date__gte=start_date).last()
 
-        # Вычисляем общее количество пропущенных дней с последней записи или с начала года
-        total_missed_days_visits = (current_date - latest_visit_report.date).days if latest_visit_report else (current_date - start_date).days
-        total_missed_days_books = (current_date - latest_book_report.date).days if latest_book_report else (current_date - start_date).days
-        total_missed_days_events = (current_date - latest_event.date).days if latest_event else (current_date - start_date).days
+        # Проверяем, были ли записи внесены или исправлены после стартовой даты
+        if latest_visit_report and latest_visit_report.date >= start_date:
+            total_missed_days_visits = 0  # Запись была внесена или исправлена
+        else:
+            total_missed_days_visits = (current_date - start_date).days
+
+        if latest_book_report and latest_book_report.date >= start_date:
+            total_missed_days_books = 0  # Запись была внесена или исправлена
+        else:
+            total_missed_days_books = (current_date - start_date).days
+
+        if latest_event and latest_event.date >= start_date:
+            total_missed_days_events = 0  # Запись была внесена или исправлена
+        else:
+            total_missed_days_events = (current_date - start_date).days
 
         # Проверяем, есть ли пропущенные дни больше CHECK_DAYS
         if total_missed_days_visits > CHECK_DAYS or total_missed_days_books > CHECK_DAYS or total_missed_days_events > CHECK_DAYS:
@@ -34,7 +44,7 @@ def check_data_fillings():
                     'books': total_missed_days_books,
                     'events': total_missed_days_events,
                 },
-                'date': target_date_only,
+                'date': start_date,
                 'is_bad_library': (total_missed_days_visits > BAD_LIBRARY_THRESHOLD or
                                    total_missed_days_books > BAD_LIBRARY_THRESHOLD or
                                    total_missed_days_events > BAD_LIBRARY_THRESHOLD)
