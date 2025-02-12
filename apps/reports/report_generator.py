b8b50054559f4d251dd1160f62f264d04777b8a3
CHECKago <mfalcon@mail.ru>
2025-02-12 18:15:53 +0300

    fixet column in diary tables

--- apps/reports/report_generator.py
+++ apps/reports/report_generator.py
@@ -23,6 +23,13 @@ def generate_visit_report_excel(user, year, month):
         date__month=month
     ).order_by('date')
 
+    # Фильтруем отчеты с начала года до текущего месяца
+    visit_reports_since_year_start = VisitReport.objects.filter(
+        library=branch,
+        date__year=year,
+        date__month__lte=month  # Все месяцы с начала года до текущего
+    ).order_by('date')
+
     # Загружаем шаблон
     base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     template_path = os.path.join(base_dir, 'reports/excell/reg_visited_template.xlsx')
@@ -36,10 +43,27 @@ def generate_visit_report_excel(user, year, month):
     # Устанавливаем год в заголовке
     ws['L1'] = f"{year} г."
 
-    # Словарь для агрегации данных по дням
+    # Словарь для агрегации данных по дням (текущий месяц)
     daily_data = {}
 
-    # Агрегируем данные по дням
+    # Словарь для агрегации данных с начала года
+    year_start_data = {
+        'qty_reg_14': 0,
+        'qty_reg_15_35': 0,
+        'qty_reg_other': 0,
+        'qty_reg_pensioners': 0,
+        'qty_reg_invalid': 0,
+        'qty_visited_14': 0,
+        'qty_visited_15_35': 0,
+        'qty_visited_other': 0,
+        'qty_visited_pensioners': 0,
+        'qty_visited_invalids': 0,
+        'qty_visited_out_station': 0,
+        'qty_visited_online': 0,
+        'note': ''
+    }
+
+    # Агрегируем данные по дням (текущий месяц)
     for report in visit_reports:
         date_key = report.date.day
         if date_key not in daily_data:
@@ -72,31 +96,49 @@ def generate_visit_report_excel(user, year, month):
         daily_data[date_key]['qty_visited_online'] += report.qty_visited_online
         daily_data[date_key]['note'] += report.note + " " if report.note else ""
 
-    # Рассчитываем итоговые значения
-    total_reg_14 = sum(data['qty_reg_14'] for data in daily_data.values())
-    total_reg_15_35 = sum(data['qty_reg_15_35'] for data in daily_data.values())
-    total_reg_other = sum(data['qty_reg_other'] for data in daily_data.values())
-    total_reg_pensioners = sum(data['qty_reg_pensioners'] for data in daily_data.values())
-    total_reg_invalid = sum(data['qty_reg_invalid'] for data in daily_data.values())
-    total_visited_14 = sum(data['qty_visited_14'] for data in daily_data.values())
-    total_visited_15_35 = sum(data['qty_visited_15_35'] for data in daily_data.values())
-    total_visited_other = sum(data['qty_visited_other'] for data in daily_data.values())
-    total_visited_pensioners = sum(data['qty_visited_pensioners'] for data in daily_data.values())
-    total_visited_invalids = sum(data['qty_visited_invalids'] for data in daily_data.values())
-    total_visited_out_station = sum(data['qty_visited_out_station'] for data in daily_data.values())
-    total_visited_online = sum(data['qty_visited_online'] for data in daily_data.values())
+    # Агрегируем данные с начала года
+    for report in visit_reports_since_year_start:
+        year_start_data['qty_reg_14'] += report.qty_reg_14
+        year_start_data['qty_reg_15_35'] += report.qty_reg_15_35
+        year_start_data['qty_reg_other'] += report.qty_reg_other
+        year_start_data['qty_reg_pensioners'] += report.qty_reg_pensioners
+        year_start_data['qty_reg_invalid'] += report.qty_reg_invalid
+        year_start_data['qty_visited_14'] += report.qty_visited_14
+        year_start_data['qty_visited_15_35'] += report.qty_visited_15_35
+        year_start_data['qty_visited_other'] += report.qty_visited_other
+        year_start_data['qty_visited_pensioners'] += report.qty_visited_pensioners
+        year_start_data['qty_visited_invalids'] += report.qty_visited_invalids
+        year_start_data['qty_visited_out_station'] += report.qty_visited_out_station
+        year_start_data['qty_visited_online'] += report.qty_visited_online
+        year_start_data['note'] += report.note + " " if report.note else ""
 
-    # Записываем данные в строку 7
+    # Записываем данные с начала года в строку 6
+    ws['B6'] = year_start_data['qty_reg_14'] + year_start_data['qty_reg_15_35'] + year_start_data['qty_reg_other']
+    ws['C6'] = year_start_data['qty_reg_14']
+    ws['D6'] = year_start_data['qty_reg_15_35']
+    ws['E6'] = year_start_data['qty_reg_other']
+    ws['F6'] = year_start_data['qty_reg_pensioners']
+    ws['G6'] = year_start_data['qty_reg_invalid']
+    ws['I6'] = year_start_data['qty_visited_14']
+    ws['J6'] = year_start_data['qty_visited_15_35']
+    ws['K6'] = year_start_data['qty_visited_other']
+    ws['L6'] = year_start_data['qty_visited_pensioners']
+    ws['M6'] = year_start_data['qty_visited_invalids']
+    ws['N6'] = year_start_data['qty_visited_out_station']
+    ws['O6'] = year_start_data['qty_visited_online']
+    ws['P6'] = year_start_data['note']
+
+    # Записываем данные в строку 7 (текущий месяц)
     row_num = 7
     first_day, first_data = next(iter(sorted(daily_data.items())))
     ws[f'A{row_num}'] = first_day
-    ws[f'B{row_num}'] = first_data['qty_reg_14'] + first_data['qty_reg_15_35'] + first_data['qty_reg_other']  # Сумма C, D, E
+    ws[f'B{row_num}'] = first_data['qty_reg_14'] + first_data['qty_reg_15_35'] + first_data['qty_reg_other']
     ws[f'C{row_num}'] = first_data['qty_reg_14']
     ws[f'D{row_num}'] = first_data['qty_reg_15_35']
     ws[f'E{row_num}'] = first_data['qty_reg_other']
     ws[f'F{row_num}'] = first_data['qty_reg_pensioners']
     ws[f'G{row_num}'] = first_data['qty_reg_invalid']
-    ws[f'H{row_num}'] = first_data['qty_visited_14'] + first_data['qty_visited_15_35'] + first_data['qty_visited_other'] + first_data['qty_visited_online']
+    ws[f'H{row_num}'] = first_data['qty_visited_14'] + first_data['qty_visited_15_35'] + first_data['qty_visited_other']
     ws[f'I{row_num}'] = first_data['qty_visited_14']
     ws[f'J{row_num}'] = first_data['qty_visited_15_35']
     ws[f'K{row_num}'] = first_data['qty_visited_other']
@@ -130,8 +172,6 @@ def generate_visit_report_excel(user, year, month):
             ws[f'N{row_num}'] = data['qty_visited_out_station']
             ws[f'O{row_num}'] = data['qty_visited_online']
             ws[f'P{row_num}'] = data['note']
-
-
 
     # Сохраняем файл в HttpResponse
     response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
