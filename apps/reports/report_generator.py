48b544416241eef69b019ff3f7b29ca5e5fae05b
Eugene Checkoy <mfalcon@mail.ru>
2025-03-10 15:37:55 +0300

    квартальный

--- apps/reports/report_generator.py
+++ apps/reports/report_generator.py
@@ -637,7 +637,8 @@ def generate_digital_month_report(user, month):
 
     ws['C5'] = sum(report.qty_reg_14 or 0 for report in visit_reports)
     ws['C6'] = sum(report.qty_reg_15_35 or 0 for report in visit_reports)
-    ws['C7'] = sum((report.qty_reg_14 or 0) + (report.qty_reg_15_35 or 0) + (report.qty_reg_other or 0) for report in visit_reports)
+    ws['C7'] = sum((report.qty_reg_14 or 0) + (report.qty_reg_15_35 or 0) + (report.qty_reg_other or 0) -
+                   (report.qty_reg_out_station or 0) for report in visit_reports)
     ws['C8'] = sum(report.qty_reg_out_of_station or 0 for report in visit_reports)
     ws['C9'] = sum((report.qty_reg_prlib or 0) + (report.qty_reg_litres or 0) for report in visit_reports)
 
