bad777d0ae02391210383923a45ab9115a119d95
Eugene Checkoy <mfalcon@mail.ru>
2025-04-04 17:11:03 +0300

    квартальный общий в свод

--- apps/reports/report_generator.py
+++ apps/reports/report_generator.py
@@ -5,6 +5,7 @@ from datetime import datetime, date
 from urllib.parse import quote
 
 import openpyxl
+from django.db.models import Q
 from django.http import HttpResponse
 from django.utils import translation
 from django.utils.formats import date_format
@@ -869,7 +870,10 @@ def generate_quarter_excel_all_branches(user, year, quarter):
     ws['W3'] = f"итого за {quarter}-й квартал {year}"
 
     # Получаем все филиалы, кроме "Администрация"
-    branches = Branch.objects.exclude(short_name__icontains='Администрация' and 'Отдел информационных технологий').order_by('short_name')
+    branches = Branch.objects.exclude(
+        Q(short_name__icontains='Администрация') |
+        Q(short_name__icontains='Отдел информационных технологий')
+    ).order_by('short_name')
 
     # Определяем месяцы квартала
     months = {
